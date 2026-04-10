80c42824d05c3deb857b9b4e57848bd945765202
Aditya Pudasaini <workingaditya00@gmail.com>
2026-04-10 08:43:35 +0545

    Added gameplay.h

---
+++ Gameplay/gameplay.h
@@ -0,0 +1,52 @@
+#ifndef GAMEPLAY_H
+#define GAMEPLAY_H
+
+#include <string>
+
+#include "../Enemy/enemy.h"
+#include "../Player/player.h"
+
+class Gameplay {
+    private:
+        Player player;
+        Enemy enemy;
+        std::string playerDecision;
+        std::string attackName;
+        std::string potionName;
+        std::string attackType;
+        
+        public:
+            Gameplay(Player player, Enemy enemy);
+
+            std::string getPlayerDecision() const;
+
+            std::string getAttackName() const;
+
+            std::string getPotionName() const;
+
+            std::string getAttackType() const;
+
+            void setPlayerDecision(std::string playerDecision);
+
+            void setAttackName(std::string attackName);
+
+            void setPotionName(std::string potionName);
+
+            void setAttackType(std::string attackType);
+
+            void attackTypeSelection();
+
+            void playerAttack();
+
+            void playerUsePotion();
+
+            float calculateDamage(float attackDamage);
+
+            void displayHealthStatus() const;
+
+            void decisionToAttackOrUsePotion();
+
+
+};
+
+#endif
