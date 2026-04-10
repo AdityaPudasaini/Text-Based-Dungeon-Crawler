750e216ef359af6ff872bd33d2dcd7dfd1dec1f9
Aditya Pudasaini <workingaditya00@gmail.com>
2026-04-10 08:42:59 +0545

    Added player.cpp

---
+++ Player/player.cpp
@@ -0,0 +1,31 @@
+#include "Player.h"
+
+Player::Player(float health, float mana, float attackDamage) {
+    this->health = health;
+    this->mana = mana;
+    this->attackDamage = attackDamage;
+}
+
+float Player::getHealth() const {
+    return health;
+}
+
+float Player::getMana() const {
+    return mana;
+}
+
+float Player::getAttackDamage() const {
+    return attackDamage;
+}
+
+void Player::setHealth(float health) {
+    this->health = health;
+}
+
+void Player::setMana(float mana) {
+    this->mana = mana;
+}
+
+void Player::setAttackDamage(float attackDamage) {
+    this->attackDamage = attackDamage;
+}
