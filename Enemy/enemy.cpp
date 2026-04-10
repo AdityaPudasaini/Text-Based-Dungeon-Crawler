2f9926a12063d7ae0c5d6aeded30f8a432fd7c68
Aditya Pudasaini <workingaditya00@gmail.com>
2026-04-10 08:41:35 +0545

    Added enemy.cpp

---
+++ Enemy/enemy.cpp
@@ -0,0 +1,29 @@
+#include "Enemy.h"
+
+Enemy::Enemy(float health, float attackDamage) {
+    this->health = health;
+    this->attackDamage = attackDamage;
+}
+
+float Enemy::getHealth() const {
+    return health;
+}
+
+float Enemy::getAttackDamage() const{
+    return attackDamage;
+}
+
+bool Enemy::getIsDefeated() const {
+    return isDefeated;
+}
+
+void Enemy::setHealth(float health) {
+    this->health = health;
+    }
+
+void Enemy::setAttackDamage(float attackDamage) {
+    this->attackDamage = attackDamage;
+}
+void Enemy::setIsDefeated(bool isDefeated) {
+    this->isDefeated = isDefeated;
+}
