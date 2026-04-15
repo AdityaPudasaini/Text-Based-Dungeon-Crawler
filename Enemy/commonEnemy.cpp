0edcde30fee20f56a6f7869a014522591b2bdabe
Aditya Pudasaini <workingaditya00@gmail.com>
2026-04-15 09:33:43 +0545

    Implement commonEnemy constructor

---
+++ Enemy/commonEnemy.cpp
@@ -0,0 +1,7 @@
+#include "commonEnemy.h"
+
+commonEnemy::commonEnemy(float health, float attackDamage, bool isDefeated) {
+    this -> health = health;
+    this -> attackDamage = attackDamage;
+    this -> isDefeated = isDefeated;
+}
